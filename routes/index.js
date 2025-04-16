1312dc2cc16b09514a8853d5dda46450296b533a
Ana <flutegurl108@hotmail.com>
2025-04-16 07:48:54 -0400

    container tests and readme

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
+const variables = {
+  inbound: 0,
+  outbound: 0
+}
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' })
+})
 
-module.exports = router;
+router.get('/all', function (req, res, next) {
+  res.send(variables)
+  //res.render('index', { title: 'Express' });
+})
+
+router.put('/add', function (req, res, next) {
+  variables.inbound++
+  res.send(variables)
+  //res.render('index', { title: 'Express' });
+})
+
+module.exports = {
+  router, variables
+}
